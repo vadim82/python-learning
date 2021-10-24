3c3fd4b930823d03352fc47c27b52cfe1c90177f
Vadim Rybak <vadim82@gmail.com>
2021-10-24 19:07:01 -0400

    added more samples of sql alchemy core

--- sql-alchemy/core_samples.py
+++ sql-alchemy/core_samples.py
@@ -3,11 +3,11 @@
 # sudo apt install libpq-dev
 
 from pprint import pprint
-from sqlalchemy import create_engine, Table, MetaData, Column
+from sqlalchemy import create_engine, Table, MetaData, Column, func
 from sqlalchemy.engine.base import Connection
 from sqlalchemy.sql.schema import Column, MetaData
 from sqlalchemy.sql.sqltypes import TIMESTAMP, Integer, String
-from sqlalchemy.sql import select
+from sqlalchemy.sql import expression, select
 
 connection_string = "postgresql://postgres:test@localhost:5432/dvdrental"
 engine = create_engine(connection_string)
@@ -15,14 +15,14 @@ engine = create_engine(connection_string)
 # conn = engine.connect()
 
 
-actors_meta = MetaData()
+metadata = MetaData()
 actors_table = Table(
-    "actor", actors_meta,
+    "actor", metadata,
     Column("actor_id", Integer, primary_key=True, nullable=False),
     Column("first_name", String(45), nullable=False),
     Column("last_name", String(45), nullable=False),
     Column("last_update", TIMESTAMP(timezone=False), nullable=False),
-),
+)
 
 
 with engine.connect() as conn:
@@ -35,7 +35,29 @@ with engine.connect() as conn:
 
     # basic select using select utility from sql alchemy
 
-    query = select(actors_table)
-    cursor = conn.execute(query)
-    result = cursor.fetchall()
-    pprint(result)
+    # query = select(actors_table)
+    # cursor = conn.execute(query)
+    # result = cursor.fetchall()
+    # pprint(result)
+
+    print(f"{actors_table.c.first_name!r}")
+    expr = actors_table.columns.actor_id > 5
+    print(expr.compile().params)
+
+
+    # 'or' and 'and' operators are '|' and '&'
+    # equality is '==' operator
+    # keep in mind that postgres is case sensitive! so you have to be careful when you query
+    q = select(actors_table) \
+        .where(actors_table.c.first_name.startswith('A')) \
+        .order_by(actors_table.c.last_name.desc()) \
+        .offset(5) \
+        .limit(5)
+    pprint(conn.execute(q).fetchall())
+
+
+    agg1 = select(func.sum(actors_table.c.actor_id))
+    print(conn.execute(agg1).scalar())
+
+
+
